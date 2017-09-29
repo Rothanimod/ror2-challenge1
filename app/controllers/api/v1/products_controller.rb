7eee32d2af33b9bbb7db003a13325662f57976d5
Mauricio Noreña <baphometh@gmail.com>
2017-09-29 12:09:32 -0500

    further testing

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -9,23 +9,26 @@ class Api::V1::ProductsController < ApiController
   def create
     product = Product.create!(product_params)
      if product.save
-        render json: { product: product, status: 201, message: "Product successfully created!"}.to_json
+        render json: { product: product, status: :created, message: "Product successfully created!"}.to_json
      else
-        render json: {errors: product.errors, status: 422, message: "name cant be blank"}.to_json
+        render json: {errors: product.errors, status: :unprocessable_entity, message: "name cant be blank"}.to_json
      end
   end
 
   def update
     product = Product.find(params[:id])
     if product.update(product_params)
-      render json: product, status: 200, message: "Product successfully updated!"
+      render json: product, status: :ok, message: "Product successfully updated!"
     else
-      render json: product.errors, status: 422, message: "name cant be blank"
+      render json: product.errors, status: :unprocessable_entity, message: "name cant be blank"
     end
   end
 
   def destroy
-
+    product = Product.find(params[:id])
+    if product.destroy
+      render json: {status: :no_content}.to_json
+    end
   end
 
   def product_params
