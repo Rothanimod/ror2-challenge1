471e150d79b9660c25d625c5c4ffa88a43ad76db
Mauricio Noreña <baphometh@gmail.com>
2017-09-29 10:06:07 -0500

    testing simplifying routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :products
   namespace :api, defaults: { format: :json }  do
       namespace :v1 do
-        resources :products, :only => [:show, :index]
+        resources :products, :only => [:index]
       end
 
     end
